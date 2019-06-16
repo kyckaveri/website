7b8a97f4e61f59dbff34f07877f083d6c6a78466
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-06-16 13:44:46 -0400

    htmlcss

--- kycwebsite/apps/publicviewcontroller/views.py
+++ kycwebsite/apps/publicviewcontroller/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 def home(request):
     context = {
         "page_name": "home",
+        "image_names": [staticfiles_storage.url("publicviewcontroller/img/home/home2.JPG"), staticfiles_storage.url("publicviewcontroller/img/home/home3.JPG")],
+        "image_iterable": range(2, 4),
     }
     return render(request, 'publicviewcontroller/home.html', context=context)
 
