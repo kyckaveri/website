5c40c8abee80d208d67d1a305c759b2db8ccd30b
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-08-22 10:44:10 -0400

    shorten member column

--- kycwebsite/apps/publicviewcontroller/views.py
+++ kycwebsite/apps/publicviewcontroller/views.py
@@ -8,6 +8,7 @@ from functools import reduce
 def home(request):
     members_list = [member for member in KYCMember.objects.all() if member.position.importance < 5]
     members_list.sort()
+    member_list = [(member.position.position_name, member.name) for member in members_list]
 
     projects_list = [project for project in Project.objects.all() if project.display]
     projects_list.sort()
@@ -18,7 +19,9 @@ def home(request):
         "page_name": "home",
         "images": [staticfiles_storage.url(f"publicviewcontroller/img/home/home{x + 1}.JPG") for x in range(2)],
         "first_image": staticfiles_storage.url("publicviewcontroller/img/home/home1.JPG"),
-        "member_info": [(member.position.position_name, member.name) for member in members_list],
+        "has_expansion": len(member_list) > 3,
+        "member_summary": member_list[:3] if len(member_list) > 3 else member_list,
+        "member_expand": member_list[3:] if len(member_list) > 3 else None,
         "hours": hours,
         "project_info": [(projects_list[i].project_name, projects_list[i].date, projects_list[i].image_url) for i in
                          range(3)],  # get only the first three projects
