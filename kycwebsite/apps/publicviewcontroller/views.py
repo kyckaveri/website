941225c4269ddbf1a9241660fd36e5a1bdef2636
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-08-24 11:19:00 -0400

    Projects desktop view

--- kycwebsite/apps/publicviewcontroller/views.py
+++ kycwebsite/apps/publicviewcontroller/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 from .models import KYCMember, Project, KYCYearSnapshot
+from .utils import get_project_obj, get_context
 
 from functools import reduce
 from datetime import datetime
@@ -13,26 +14,23 @@ def home(request):
     members_list.sort()
     member_list = [(member.position.position_name, member.name) for member in members_list]
 
-    projects_list = [project for project in Project.objects.all() if project.display]
-    projects_list.sort()
+    projects_list = [project for project in Project.objects.all().order_by('-date') if project.display]
     hours = reduce(lambda x, y: x + y, [project.hours * project.members_attended for project in projects_list])
 
-    login_name = 'Login'
-    if request.user.is_authenticated:
-        login_name = request.user
+    context = get_context(request, 'home')
 
-    context = {
-        "page_name": "home",
-        "images": [staticfiles_storage.url(f"publicviewcontroller/img/home/home{x + 1}.JPG") for x in range(2)],
-        "first_image": staticfiles_storage.url("publicviewcontroller/img/home/home1.JPG"),
-        "has_expansion": len(member_list) > 3,
-        "member_summary": member_list[:3] if len(member_list) > 3 else member_list,
-        "member_expand": member_list[3:] if len(member_list) > 3 else None,
-        "hours": hours,
-        "project_info": [(projects_list[i].project_name, projects_list[i].date, projects_list[i].image_url) for i in
-                         range(3)],  # get only the first three projects,
-        "login_name": login_name
-    }
+    context["first_image"] = staticfiles_storage.url("publicviewcontroller/img/home/home1.JPG")
+    context["images"] = [staticfiles_storage.url(f"publicviewcontroller/img/home/home{x + 1}.JPG") for x in range(2)]
+
+    context["has_expansion"] = len(member_list) > 3
+    context["member_summary"] = member_list[:3] if len(member_list) > 3 else member_list
+    context["member_expand"] = member_list[3:] if len(member_list) > 3 else None
+
+    context["hours"] = hours
+    context["projects"] = [get_project_obj(projects_list[i]) for i in range(4)]  # get only the first four projects,
+    context["columns"] = [[get_project_obj(projects_list[i]) for i in range(0, 2)],
+                          [get_project_obj(projects_list[i]) for i in range(2, 4)]]  # get only the first four projects,
+
     return render(request, 'publicviewcontroller/home.html', context=context)
 
 
@@ -41,10 +39,6 @@ def members(request):
 
 
 def members_by_year(request, year):
-    login_name = 'Login'
-    if request.user.is_authenticated:
-        login_name = request.user
-
     snapshots = KYCYearSnapshot.objects.order_by('-year')
     years = [snapshot.year for snapshot in snapshots]
 
@@ -53,34 +47,22 @@ def members_by_year(request, year):
     except KYCYearSnapshot.DoesNotExist:
         return HttpResponseRedirect(reverse('publicviewcontroller:members'))
 
-    context = {
-        "page_name": "members",
-        "snapshot": snapshot,
-        "years": years,
-        "year": year,
-        "login_name": login_name
-    }
+    context = get_context(request, "members")
+    context["snapshot"] = snapshot
+    context["years"] = years
+    context["year"] = year
 
     return render(request, 'publicviewcontroller/members.html', context=context)
 
 
 def projects(request):
-    login_name = 'Login'
-    if request.user.is_authenticated:
-        login_name = request.user
-
     column_one = []
     column_two = []
     column_three = []
     all_projects = []
 
     for index, project in enumerate([p for p in Project.objects.all().order_by('-date') if p.display]):
-        project_obj = {
-            "NAME": project.project_name,
-            "IMAGE": project.image_url,
-            "VOLUNTEERS": project.members_attended,
-            "DATE": f"{project.date.month}/{project.date.day}/{str(project.date.year)[2:]}"
-        }
+        project_obj = get_project_obj(project)
         all_projects.append(project_obj)
         if index % 3 == 0:
             column_one.append(project_obj)
@@ -89,10 +71,7 @@ def projects(request):
         elif index % 3 == 2:
             column_three.append(project_obj)
 
-    context = {
-        "page_name": "projects",
-        "columns": [column_one, column_two, column_three],
-        "all_projects": all_projects,
-        "login_name": login_name,
-    }
+    context = get_context(request, 'projects')
+    context["columns"] = [column_one, column_two, column_three]
+    context["all_projects"] = all_projects
     return render(request, 'publicviewcontroller/projects.html', context=context)
