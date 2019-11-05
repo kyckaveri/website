3fd4e8c8be4dd99466c35471ee4581bf2bbf43ca
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-11-04 22:22:58 -0500

    show junior members on frontpage

--- kycwebsite/apps/publicviewcontroller/views.py
+++ kycwebsite/apps/publicviewcontroller/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import KYCMember, Project, KYCYearSnapshot, CarouselImage
+from .models import KYCMember, Project, KYCYearSnapshot, CarouselImage, KYCJuniorMember, JuniorPosition
 from .utils import get_project_obj, get_context
 
 from datetime import datetime
@@ -13,9 +13,15 @@ def home(request):
     members_list.sort()
     member_list = [(member.position.position_name, member.name) for member in members_list]
 
+    junior_members_list = [member for member in KYCJuniorMember.objects.all().filter(deleted=False) if
+                           member.position.importance is not 100]
+    junior_members_list.sort()
+    junior_members_list = [(member.position.position_name, member.name) for member in junior_members_list]
+
     projects_list = [project for project in Project.objects.all().filter(deleted=False).order_by('-date') if
                      project.display]
-    hours = int(sum([project.hours * project.members_attended for project in [project for project in Project.objects.all().filter(deleted=False)]]))
+    hours = int(sum([project.hours * project.members_attended for project in
+                     [project for project in Project.objects.all().filter(deleted=False)]]))
 
     images = [image.image_url for image in CarouselImage.objects.all().filter(deleted=False).order_by('index')]
 
@@ -24,16 +30,23 @@ def home(request):
     context["first_image"] = images[0] if len(images) > 0 else ""
     context["images"] = images
 
-    context["has_expansion"] = len(member_list) > 3
     context["member_summary"] = member_list[:3] if len(member_list) > 3 else member_list
     context["member_expand"] = member_list[3:] if len(member_list) > 3 else None
+
+    if context["member_expand"] is None:
+        context["member_expand"] = junior_members_list if len(junior_members_list) > 0 else None
+    else:
+        context["member_expand"].extend(junior_members_list)
+
+    context["has_expansion"] = len(context["member_expand"]) > 0
 
     context["hours"] = hours if hours > 0 else 1
 
     try:
         context["projects"] = [get_project_obj(projects_list[i]) for i in range(4)]  # get only the first four projects,
         context["columns"] = [[get_project_obj(projects_list[i]) for i in range(0, 2)],
-                              [get_project_obj(projects_list[i]) for i in range(2, 4)]]  # get only the first four projects
+                              [get_project_obj(projects_list[i]) for i in
+                               range(2, 4)]]  # get only the first four projects
     except IndexError:
         context["projects"] = []
         context["columns"] = []
