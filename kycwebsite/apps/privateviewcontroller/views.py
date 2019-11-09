4f8dc032e34c345caf703e3dee91fc0a455a5ed6
=Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-11-09 10:32:40 -0500

    fix

--- kycwebsite/apps/privateviewcontroller/views.py
+++ kycwebsite/apps/privateviewcontroller/views.py
@@ -116,13 +116,17 @@ def add_member(request):
 
     try:
         position = Position.objects.filter(position_name=position_name).first()
+        x = []
+        if position is None:
+            for p in Position.objects.all():
+                x.append((p.position_name, p.position_name == position_name))
         new_member = KYCMember(name=name, position=position)
         new_member.save()
     except:
         return HttpResponseRedirect(
             reverse(
                 "privateviewcontroller:admindashboard",
-                kwargs={"message": f"Error creating new member: {str(list(Position.objects.filter(position_name=position_name)))}"},
+                kwargs={"message": f"Error creating new member: {str(x)}"},
             )
         )
 
